8281d6ce11a12ee8129702e2aa8af500be1ad3dc
VictorHugoRaisCardoso <victornegocio.expert@gmail.com>
2023-07-26 13:22:23 -0300

    Atualizando o ex067
    O exercício não estava salvo em lugar nenhum, refiz e disponibilizei junto com os demais exercícios

--- Mundo-2/ex067.py
+++ Mundo-2/ex067.py
@@ -2,9 +2,11 @@ from time import sleep
 
 while True:
     tabuada = int(input('Informe um valor negativo para interromper\nA tabuada de que numero você gostaria de ver agora? '))
+    print('=' * 60)
     if tabuada < 0:
         break
     else:
         for numero in range(0, 11):
             sleep(1)
-            print(f'{numero:>2} x {tabuada:>2} = {tabuada * numero:>2}')
+            print(f'{numero:>2} x {tabuada:>2} = {tabuada * numero:>3}')
+    print('=' * 60)
